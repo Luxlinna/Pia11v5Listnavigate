109cf13ce2b80844e7f5191e838fa1bb33e1607e
luxlinna <luxlinna@gmail.com>
2022-10-13 09:10:12 +0200

    Initial Commit

---
+++ Pia11v5Listnavigate/Pia11v5ListnavigateApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pia11v5ListnavigateApp.swift
+//  Pia11v5Listnavigate
+//
+//  Created by Linna Lux on 2022-10-13.
+//
+
+import SwiftUI
+
+@main
+struct Pia11v5ListnavigateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
